bfa413e12b96492ebc0ba33f5027ebd88d7abaad
Cristian Arias <cristian_ariaschaves@hotmail.com>
2024-05-18 22:22:37 -0600

    Add files to train with CPU

---
+++ run_ai_bench_cpu.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from ai_benchmark import AIBenchmark
+
+benchmark = AIBenchmark(use_CPU=True, verbose_level=3)
+
+start_now = datetime.now()
+start_current_time = start_now.strftime("%H:%M:%S")
+print("Start Time =", start_current_time)
+benchmark.run(precision="normal")
+end_now = datetime.now()
+end_current_time = end_now.strftime("%H:%M:%S")
+print("End Time =", end_current_time)
+diff = end_now-start_now
+diff_minutes = diff.total_seconds() / 60
+print("Execution time in minutes =", diff_minutes)
